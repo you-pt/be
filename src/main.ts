ffaa8c66971f14d77ee9dd82daf76d99ce4615b9
lyukidon <qgx99098@gmail.com>
2024-04-29 15:24:17 +0900

    [B-Fix] ADD CORS OPTION

--- src/main.ts
+++ src/main.ts
@@ -14,15 +14,18 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     //<-- NestExpressApplication은 ejs확인 위해 추가
     logger: winstonLogger,
-    cors: true,
   });
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
     }),
   );
+  console.log(process.env.BASE_URL);
 
-  app.enableCors();
+  app.enableCors({
+    origin: [process.env.BASE_URL],
+    credentials: true,
+  });
   const config = new DocumentBuilder()
     .setTitle('YOU-PT API')
     .setDescription('YOU-PT API 테스트 입니다.')
