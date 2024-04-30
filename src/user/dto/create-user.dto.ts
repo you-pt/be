dba202de52b56ef435a6e5f213fd6d921c444071
lyukidon <qgx99098@gmail.com>
2024-04-30 11:38:32 +0900

    [B-Fix] 로그인 필수 사항 일부 제거

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -21,21 +21,21 @@ export class CreateUserDto {
     @IsNotEmpty({ message: '닉네임을 입력해주세요.' })
     nickname: string;
   
-    @IsString()
-    @IsNotEmpty({ message: '이름을 입력해주세요.' })
-    name: string;
+    // @IsString()
+    // @IsNotEmpty({ message: '이름을 입력해주세요.' })
+    // name: string;
 
     @IsEnum(Gender)
     @IsNotEmpty({ message: '성별을 입력해주세요.' })
     gender: Gender;
   
-    @IsNotEmpty({ message: '전화번호를 입력해주세요.' })
-    @Matches(/^\d{3}-\d{3,4}-\d{4}$/)
-    phone: string;
+    // @IsNotEmpty({ message: '전화번호를 입력해주세요.' })
+    // @Matches(/^\d{3}-\d{3,4}-\d{4}$/)
+    // phone: string;
 
-    @IsNotEmpty({ message: '생년월일을 입력해주세요.' })
-    @Matches(/^\d{4}-\d{1,2}-\d{1,2}$/)
-    birth: string;
+    // @IsNotEmpty({ message: '생년월일을 입력해주세요.' })
+    // @Matches(/^\d{4}-\d{1,2}-\d{1,2}$/)
+    // birth: string;
   
     @IsEnum(Role)
     role: Role;
